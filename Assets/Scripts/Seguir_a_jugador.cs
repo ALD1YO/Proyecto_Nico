1a3cea3004e8718fd7e4a9c4b4a3d69f5bc1f3f1
ALD1YO <crespoaldo29@gmail.com>
2018-05-11 14:21:42 -0500

    Commit con la ayuda del codigo de Navmesh de Mario

--- Assets/Scripts/Seguir_a_jugador.cs
+++ Assets/Scripts/Seguir_a_jugador.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Seguir_a_jugador : MonoBehaviour {
 	public Transform[] target;
 	public float speed;
-
+	NavMeshAgent agent;
 	private int current;
-	
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+	}
 	// Update is called once per frame
 	void Update () {
-		if (transform.position != target [current].position) {
+		/*if (transform.position != target [current].position) {
 			Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
 			GetComponent<Rigidbody>().MovePosition(pos); 
 		} else
 			current = (current + 1)%target.Length;
+			*/
+		if(Vector3.Distance(transform.position, target[0].position) < 4){
+			agent.SetDestination(target[0].position);
+			agent.isStopped = false;
+		}
+		else{
+			agent.isStopped = true;
+		}
 	}
 }
