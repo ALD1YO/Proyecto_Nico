6a5c1f8734ef1d68c29b5fabd87eae1f2eea0fc5
ALD1YO <crespoaldo29@gmail.com>
2018-05-19 23:23:51 -0500

    Se redireccionan assets en carpetas y se agrega un menu principal piloto funcionable.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+public void PlayGame(){
+	SceneManager.LoadScene
+	(SceneManager.GetActiveScene().buildIndex +1);
+	//This shit loads the next level in the queue
+}
+
+public void QuitGame(){
+	Debug.Log("QUIT");
+	Application.Quit();
+}
+
+}
+
+/*****************RULES****************/
+/*Everytime I want to change scenes in Unity,
+Include
+using UnityEngine.SceneManagement;
+*/
---
+++ Assets/Scripts/Scripts_InGame/Sonido_Ventana.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonido_Ventana : MonoBehaviour {
+
+	// Use this for initialization
+	bool Repetir = false;
+	public AudioClip MusicClip;
+
+	public AudioSource MusicSource;
+	void Start () {
+		MusicSource.clip = MusicClip;
+	}
+	
+	
+/* 
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			MusicSource.Play();
+		}
+	}
+*/
+	void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "RigidBodyFPSController" && Repetir ==false)
+        {
+            Debug.Log("Colision detectada");
+            MusicSource.Play();
+			Repetir = true;
+        }
+        /*
+        GameObject otherObj = collision.gameObject;
+        Debug.Log("Collided with: " + otherObj);
+        */
+    }
+}
