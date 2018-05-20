0fd97d10e28bad668792eba578666bb78b6de744
ALD1YO <crespoaldo29@gmail.com>
2018-05-20 12:26:12 -0500

    Se agregan materiales a la muñeca. Se cambia el skybox. Se prueba un script experimental para recargar la escena.

---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ReloadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")){
+			print("space key was pressed");
+			SceneManager.LoadScene("The_Room");
+			//Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
--- Assets/Scripts/Scripts_InGame/Collision_Test.cs
+++ Assets/Scripts/Scripts_InGame/Collision_Test.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Collision_Test : MonoBehaviour {
 
+    public GameObject target;
 
+    void Start(){
+        //agent = GetComponent<NavMeshAgent>();
+    }
+    
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.name == "Shrek_prueba")
         {
-            Debug.Log("Colision detectada");
+            Debug.Log("Colision detectada con Shrek");
             Destroy(col.gameObject);
+        }
+
+        if(col.gameObject.name == "Doll_2"){
+            Debug.Log("La mona te atrapo");
+            Destroy(target);
         }
         /*
         GameObject otherObj = collision.gameObject;
         Debug.Log("Collided with: " + otherObj);
         */
     }
-    
+    /*
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.name == "Shrek_prueba")
@@ -26,5 +36,6 @@ public class Collision_Test : MonoBehaviour {
             Destroy(col.gameObject);
         }
     }
+     */
     
 }
