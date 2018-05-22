65a8522a39ceddeba83417cf8644e97f3ad339a1
ALD1YO <crespoaldo29@gmail.com>
2018-05-22 01:17:31 -0500

    Se agrega la música piloto para que aparezca el muñeco. Se implementó que el muñeco apareciera después de reproducir la música.

---
+++ Assets/Scripts/Scripts_InGame/Song_Delay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Song_Delay : MonoBehaviour {
+
+	AudioSource myAudio;
+	public GameObject target;
+	public float Time_to_Start= 1.0f;
+	// Use this for initialization
+	void Start () {
+
+		myAudio = GetComponent<AudioSource>();
+		myAudio.PlayDelayed(Time_to_Start);
+		//target.SetActive(false);
+	}
+
+	void Update(){
+		if(!myAudio.isPlaying){
+			Debug.Log("El Audio no suena");
+			target.SetActive(true);
+		}
+
+
+	}
+}
